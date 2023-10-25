2144d21a15675d118f4ffefab0b0a4a55760d8f2
Muffinney <mf101408@wgcloud.org>
2023-10-25 11:31:27 -0500

    Added code to convert dollars to euros

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# DollarsToEuros
+# Dollars To Euros Converter
+### Version 1.0
+Convert an amount of dollars into euros
+Program by Matthew Finney
---
+++ dollarstoeuros.py
@@ -0,0 +1,14 @@
+while True:
+    # Asks for dollar amount
+    dollar = float(input("Enter dollar amount to be converted: "))
+
+    # Conversion amount
+    euros = dollar * .94540
+
+    # Output euro amount
+    print("You have €" + str(round(euros, 2)))
+
+    # Asks if user wants to continue
+    convert = input("Would you like to convert dollars to euros again? ")
+    if convert == "no":
+        break
